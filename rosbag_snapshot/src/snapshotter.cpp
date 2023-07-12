ef76a9d2fd2658909c98134fc83291427b4295de
Noah LeBlanc <nleblanc@locusrobotics.com>
2023-07-12 16:29:49 -0400

    Added more debug messages

--- rosbag_snapshot/src/snapshotter.cpp
+++ rosbag_snapshot/src/snapshotter.cpp
@@ -421,11 +421,14 @@ bool Snapshotter::writeTopic(rosbag::Bag& bag, MessageQueue& message_queue, stri
   {
     ros::Time start = req.start_time;
     bool latched = message_queue.isLatched();
+    // ROS_WARN_STREAM("Bagging messages for topic " << message_queue.)
 
     if (start == ros::Time(0))
     {
       start = now - message_queue.options_.duration_limit_;
     }
+
+    ROS_WARN_STREAM("Set start time to " << start);
 
     std::vector<std::string> callers;
     for (MessageQueue::range_t::first_type msg_it = range.first; msg_it != range.second; ++msg_it)
@@ -434,6 +437,7 @@ bool Snapshotter::writeTopic(rosbag::Bag& bag, MessageQueue& message_queue, stri
       // Latched messages can have old timestamps so set the timestamp to the bag start time in this case
       if (msg.time < start)
       {
+        ROS_WARN_STREAM("Updating old timestamp " << msg.time);
         msg.time = start;
       }
       bag.write(topic, msg.time, msg.msg, msg.connection_header);
